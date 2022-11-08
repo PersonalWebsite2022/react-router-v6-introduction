82d88bf6d62a3d065a4569e2d53cd7c475da5038
PersonalWebsite2022 <PersonalWebsite2022@gmail.com>
2022-11-08 11:32:58 -0800

    React Router V6 Introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="about" element={ <About/> } />
+        <Route path="contact" element={ <Contact/> } />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+const Home = () => {
+  return(
+  <div>
+      <h1>This is home page</h1>
+      <Link to="/about">Click to view our about page</Link>
+      <br/>
+      <Link to="/contact">Click to view our contact page</Link>
+    </div>
+  )
+}
+
+const Contact = () => {
+  return(
+    <div>
+      <h1>This is contact page</h1>
+      <Link to="/">Click to view our home page</Link>
+      <br/>
+      <Link to="/about">Click to view our about page</Link>
+    </div>
+    )
+}
+
+const About = () => {
+  let location = useLocation();
+  let { from, pathname } = location;
+  return(
+    <div>
+      <h1>This is about page</h1>
+      <Link to="/">Click to view our home page</Link>
+      <br/>
+      <Link to="/contact">Click to view our contact page</Link>
+      <div>You are currently at {pathname}.</div>
+    </div>
+    )
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
     </div>
   );
 }
